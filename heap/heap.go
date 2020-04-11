6f15267fad9ff5cfdc96454c9cc2c507a9f38eef
Jimmy Huang <racketprogram@gmail.com>
2020-04-12 02:43:12 +0800

    Sort slice by poll

--- heap/heap.go
+++ heap/heap.go
@@ -1,8 +1,5 @@
 package heap
 
-import (
-)
-
 type Heap struct {
 	maxSize      int
 	currentIndex int
@@ -21,7 +18,6 @@ func (h *Heap) Insert(v int) {
 	if h.currentIndex >= h.maxSize {
 		return
 	}
-
 	h.array[h.currentIndex] = v
 	h.Max(h.currentIndex)
 	h.currentIndex++
@@ -33,9 +29,59 @@ func (h *Heap) Max(idx int) {
 	}
 	parentIdx := (idx / 2)
 	if h.array[parentIdx] < h.array[idx] {
-		tmp := h.array[parentIdx]
-		h.array[parentIdx] = h.array[idx]
-		h.array[idx] = tmp
+		swap(&h.array[parentIdx], &h.array[idx])
 		h.Max(parentIdx)
 	}
+}
+
+func (h *Heap) Poll() int {
+	pop := h.array[0]
+	swap(&h.array[0], &h.array[h.currentIndex-1])
+	h.array[h.currentIndex-1] = 0
+	h.currentIndex--
+	h.Min(0)
+	return pop
+}
+
+func (h *Heap) Min(idx int) {
+	child1 := (idx * 2) + 1
+	child2 := (idx * 2) + 2
+
+	c1, c2 := 0, 0
+	if child1 > h.currentIndex {
+		c1 = -9999999
+	} else {
+		c1 = h.array[child1]
+	}
+	if child2 > h.currentIndex {
+		c2 = -9999999
+	} else {
+		c2 = h.array[child2]
+	}
+
+	c := 0
+	cIndex := 0
+	if c1 < c2 {
+		c = c2
+		cIndex = child2
+	} else {
+		c = c1
+		cIndex = child1
+	}
+
+	if c > h.array[idx] {
+		swap(&h.array[cIndex], &h.array[idx])
+		h.Min(cIndex)
+	}
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func swap(a, b *int) {
+	*a, *b = *b, *a
 }
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -16,6 +16,21 @@ func TestNewHeapInsert(t *testing.T) {
 	for _, v := range s {
 		heap.Insert(v)
 	}
-	
-	fmt.Printf("%+v\n", heap)
+
+	fmt.Printf("TestNewHeapInsert: %+v\n", heap)
+}
+
+func TestNewHeapPoll(t *testing.T) {
+	s := []int{2, 10, 5, 1, 7, 9, 3, 4, 6, 8}
+	heap := NewHeap(len(s))
+	for _, v := range s {
+		heap.Insert(v)
+	}
+	fmt.Printf("TestNewHeapPoll %+v\n", heap)
+	var sorted []int
+	for i := 0; i < 10; i ++{
+		sorted = append(sorted, heap.Poll())
+		fmt.Printf("TestNewHeapPoll %+v\n", heap)
+	}
+	fmt.Printf("TestNewHeapPoll sorted %+v\n", sorted)
 }
