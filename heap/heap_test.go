6e25f53f8d13c66d9dbd4eb81c2b42a4cb51ccd6
Jimmy Huang <racketprogram@gmail.com>
2020-04-13 22:52:06 +0800

    Add go mod

--- heap/heap_test.go
+++ heap/heap_test.go
@@ -3,6 +3,8 @@ package heap
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewHeap(t *testing.T) {
@@ -28,9 +30,10 @@ func TestNewHeapPoll(t *testing.T) {
 	}
 	fmt.Printf("TestNewHeapPoll %+v\n", heap)
 	var sorted []int
-	for i := 0; i < 10; i ++{
+	for i := 0; i < 10; i++ {
 		sorted = append(sorted, heap.Poll())
 		fmt.Printf("TestNewHeapPoll %+v\n", heap)
 	}
 	fmt.Printf("TestNewHeapPoll sorted %+v\n", sorted)
+	assert.Equal(t, sorted, []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1})
 }
