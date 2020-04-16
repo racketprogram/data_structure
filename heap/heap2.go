f029b1246dcd608da8d0fc27d73582a34fc576db
Jimmy Huang <jimmy.huang@jkopay.com>
2020-04-16 14:05:42 +0800

    Update heap2

--- heap/heap2.go
+++ heap/heap2.go
@@ -1,25 +1,30 @@
 package heap
 
+import "fmt"
+
+func init() {
+	fmt.Println("heap2 init")
+	fmt.Println(9 / 2)
+}
+
 type Heap2 struct {
 	max   int
 	array []int
+	lastIndex int
 }
 
 func NewHeap2(a []int) Heap2 {
 	return Heap2{
 		max:   256,
 		array: a,
+		lastIndex: len(a) - 1,
 	}
 }
 
-func (h *Heap2) lastIndex() int {
-	return len(h.array) - 1
-}
-
 func (h *Heap2) build() {
-	halfIndex := (h.lastIndex() / 2) - 1
+	halfIndex := (h.lastIndex / 2)
 
-	for i := halfIndex; i <= halfIndex*2; i++ {
+	for i := halfIndex; i >= 0; i-- {
 		h.heapify(halfIndex)
 	}
 }
@@ -28,24 +33,34 @@ func (h *Heap2) heapify(i int) {
 	c1 := i*2 + 1
 	c2 := i*2 + 2
 
-	if h.array[c1] < h.array[i] || h.array[c2] < h.array[i] {
-		if h.array[c2] > h.array[c2] {
-			h.array[i], h.array[c2] = h.array[c2], h.array[i]
+	if c2 > h.lastIndex {
+		if h.array[c1] < h.array[i] {
+			h.arraySwap(i, c1)
+		}
+	} else if h.array[c1] < h.array[i] || h.array[c2] < h.array[i] {
+		if h.array[c1] > h.array[c2] {
+			h.arraySwap(i, c2)
 		} else {
-			h.array[i], h.array[c1] = h.array[c1], h.array[i]
+			h.arraySwap(i, c1)
 		}
 	}
 
+	// FIXME
 	if i > 0 {
 		parent := i / 2
 		h.heapify(parent)
 	}
 }
 
-// [5, 1, 4, 2, 3, 9, 7]
-// half := 1
-//
-// 		   5
-// 	   1       4
-//  2   3    9   7
-//
+func (h *Heap2) Poll(i int) int {
+	h.arraySwap(0, h.lastIndex)
+	return 0
+}
+
+func (h *Heap2) bubbleDown(i int) {
+
+}
+
+func (h *Heap2) arraySwap(i, j int) {
+	h.array[i], h.array[j] = h.array[j], h.array[i]
+}
