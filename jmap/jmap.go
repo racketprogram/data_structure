f02e298a15bebf082e599ec004f97474a73bcc78
Jimmy Huang <jimmy.huang@jkopay.com>
2020-04-30 13:13:21 +0800

    Add Get of map

--- jmap/jmap.go
+++ jmap/jmap.go
@@ -1,8 +1,14 @@
 package jmap
 
 type jmap struct {
-	s []string
-	h *hash
+	s           []*kv
+	h           *hash
+	valuesCount int
+}
+
+type kv struct {
+	k string
+	v string
 }
 
 func NewJmap(size int) (*jmap, error) {
@@ -12,7 +18,39 @@ func NewJmap(size int) (*jmap, error) {
 	}
 
 	return &jmap{
-		s: make([]string, 0, size),
+		s: make([]*kv, size),
 		h: h,
 	}, nil
+}
+
+func (m *jmap) Set(key, value string) {
+	if m.valuesCount > len(m.s)-1 {
+		panic("full")
+	}
+	hashValue := m.h.stringHash(key)
+	for {
+		if m.s[hashValue] == nil {
+			m.s[hashValue] = &kv{k: key, v: value}
+			m.valuesCount++
+			return
+		}
+		hashValue++
+		if hashValue > len(m.s)-1 {
+			hashValue = 0
+		}
+	}
+}
+
+func (m *jmap) Get(key string) (value string, ok bool) {
+	hashValue := m.h.stringHash(key)
+	for {
+		v := m.s[hashValue]
+		if v != nil && v.k == key {
+			return v.v, true
+		}
+		hashValue++
+		if hashValue > len(m.s)-1 {
+			hashValue = 0
+		}
+	}
 }
--- jmap/jmap_test.go
+++ jmap/jmap_test.go
@@ -1,6 +1,8 @@
 package jmap
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,4 +12,54 @@ func TestNewJMap(t *testing.T) {
 	h, err := NewJmap(256)
 	assert.NoError(t, err)
 	assert.NotEqual(t, h, nil)
+}
+
+func TestFailToNewJMap(t *testing.T) {
+	_, err := NewJmap(255)
+	assert.Error(t, err)
+}
+
+func TestSetJMap(t *testing.T) {
+	m, err := NewJmap(256)
+	assert.NoError(t, err)
+	m.Set("1", "a")
+	fmt.Printf("map: %+v:", m)
+}
+
+func TestSetJMapCollision(t *testing.T) {
+	m, err := NewJmap(256)
+	assert.NoError(t, err)
+	m.Set("12", "a")
+	m.Set("21", "b")
+	fmt.Printf("map: %+v:", m)
+}
+
+func TestSetJMapFull(t *testing.T) {
+	m, err := NewJmap(256)
+	assert.NoError(t, err)
+	for i := 0; i < 256; i++ {
+		m.Set(strconv.Itoa(i), "a")
+	}
+}
+
+func TestSetJMapFullPlusOne(t *testing.T) {
+	m, err := NewJmap(256)
+	assert.NoError(t, err)
+	for i := 0; i < 257; i++ {
+		m.Set(strconv.Itoa(i), "a")
+	}
+	fmt.Printf("map: %+v:", m)
+}
+
+func TestGetJMap(t *testing.T) {
+	m, err := NewJmap(256)
+	assert.NoError(t, err)
+	for i := 0; i < 256; i++ {
+		m.Set(strconv.Itoa(i), strconv.Itoa(i))
+	}
+	for i := 0; i < 256; i++ {
+		v, ok := m.Get(strconv.Itoa(i))
+		assert.True(t, ok)
+		assert.Equal(t, v, strconv.Itoa(i))
+	}
 }
